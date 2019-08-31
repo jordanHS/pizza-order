051df4fc0d515bb198d745129c11eeff4aab333c
Jordan Safford <jordansafford@gmail.com>
2019-08-30 17:05:34 -0700

    add business logic for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,26 @@ function Pizza (size, toppings, toppingCost) {
 
 }
 
-Size.prototype.cost = function() {
+Pizza.prototype.cost = function() {
   if (size = "small") {
-    return = "$10";
-  else  (size = "medium") {
-    return = "$12";
-} else (size = "large") {
-  return = "$15";
-} else (size = "extra-large") {
-  return = "$20"
+    return  "$10";
+} else if (size = "medium") {
+    return  "$12";
+} else if (size = "large") {
+  return  "$15";
+} else if (size = "extra-large") {
+  return  "$20"
+  }
 }
+
+Toppings.prototype.cost = function() {
+  if ("toppings".length[] = "1") {
+    return "$1";
+}  else  if (toppings.length[] = "2") {
+  return "$2";
+} else if (toppings.length)
+
+
 
 
 
@@ -23,7 +33,14 @@ $(document).ready(function() {
   $(".form-one").submit(function(event) {
     $(".form-one").hide();
 })
-  $(".form-two").submit(function(eve) {
+  $(".form-two").submit(function(event) {
     $(".form-two").show();
+      var inputtedSize = $("input#size").val();
+        $("input#size").val("");
+      var inputtedToppings = [];
+      $("input:checkbox[name=toppings]:checked").each(function(){
+        var Toppings = $(this).val();
+        inputtedToppings.push(Toppings)
+      });
 })
 });
