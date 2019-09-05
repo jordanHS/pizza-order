e2801b59712d0dda79531c94310a7b6c635517c5
Student <example@example.com>
2019-09-05 16:44:07 -0700

    fix Javasscript errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function Pizza (size, toppings, toppingCost) {
+function Pizza (size, toppings) {
   this.size = size;
   this.toppings = toppings;
 
@@ -7,9 +7,9 @@ function Pizza (size, toppings, toppingCost) {
 Pizza.prototype.size = function() {
   if (size = "small") {
     return  "$10";
-} else if (size = "medium") {
+  } else if (size = "medium") {
     return  "$12";
-} else if (size = "large") {
+  } else if (size = "large") {
     return  "$15";
   }
 }
@@ -17,8 +17,9 @@ Pizza.prototype.size = function() {
 Pizza.prototype.toppings = function() {
   if ("toppings".length = 1) {
     return "$1";
-}  else  if ("toppings".length = 2) {
+  }  else  if ("toppings".length = 2) {
     return "$2";
+  }
 }
 
 Pizza.prototype.total = function() {
@@ -32,14 +33,19 @@ Pizza.prototype.total = function() {
 
 
 $(document).ready(function() {
-  $(".form-one").submit(function(event) {
-      var inputtedSize = $("input#size").val();
-        $("input#size").val("");
-      var inputtedToppings = [];
-      $("input:checkbox[name=toppings]:checked").each(function(){
-        var Toppings = $(this).val();
-        inputtedToppings.push(Toppings)
-      });
+  $(".pizza").submit(function(event) {
+    $("#price-one").show();
+    $("#price-two").show();
+    $("#price-three").show();
+    event.preventDefault();
+    var inputtedSize = $("input:radio[name=size]").val();
+    $("input:radio[name=size]").val("");
+    console.log(inputtedSize);
+    var inputtedToppings = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var Toppings = $(this).val();
+      inputtedToppings.push(Toppings)
     })
-  })
-}
+    var newPizza = new Pizza(inputtedSize, inputtedToppings);
+  });
+});
